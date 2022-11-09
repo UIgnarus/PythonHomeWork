b75a08201602c5fca8b9e9c4b88ce7247303f5de
Ignarus <kitoroli98@gmail.com>
2022-11-09 21:31:28 +0300

    save

--- Program.py
+++ Program.py
@@ -1,19 +1,12 @@
-"""
-1 Напишите программу, которая принимает
- на вход вещественное число и показывает сумму его цифр.
-Пример:
-- 6782 -> 23
-- 0,56 -> 11
-"""
-n = (input("n = "))
+n = round(float(input()), 4)
 
 def numbers_sum(x):
     sum = 0
-    for i in x:
+    for i in str(x):
         if i.isdigit():
             sum+=int(i)
     return sum
 
-print(numbers_sum(n))
+print(int(numbers_sum(n)))
 
 
