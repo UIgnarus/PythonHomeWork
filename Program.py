ed9c8c398996a5b6016323c4619ac3a9756cb104
Ignarus <kitoroli98@gmail.com>
2022-11-09 21:42:17 +0300

    save

--- Program.py
+++ Program.py
@@ -1,12 +1,6 @@
 
 n = int(input())
-
-def factorial_seqance(x):
-    a = [1]
-    result = "1"
-    for i in range(2, x + 1):
-        a.append(i*a[i-2])
-        result += " " + str(a[i-1]) 
-    return result
-
-print(factorial_seqance(n))
+x = 1
+for i in range(1, n+1):
+    x*=i
+    print(int(x), end = " ")
