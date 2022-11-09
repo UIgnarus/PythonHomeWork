2c9eec5c4bc1271eb5a5933371f8a2bb446d9195
Ignarus <kitoroli98@gmail.com>
2022-11-09 23:04:38 +0300

    save

--- Program.py
+++ Program.py
@@ -1,8 +1,5 @@
 array = [int(i) for i in input().split()]
 for i in range(len(array)- 1):
     array[i], array[i+1] = array[i+1], array[i]
-
-for i in range(len(array)):
     print(int(array[i]), end = " ")
-
-
+print(array[-1])
