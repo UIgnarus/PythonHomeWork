60477528edab37f8f875befe3f280b7eb2695342
Ignarus <kitoroli98@gmail.com>
2022-11-09 22:23:08 +0300

    save

--- Program.py
+++ Program.py
@@ -1,12 +1,9 @@
 n = round(float(input()), 4)
 
-def numbers_sum(x):
-    sum = 0
-    for i in str(x):
-        if i.isdigit():
-            sum+=int(i)
-    return sum
-
-print(int(numbers_sum(n)))
+sum = 0
+for i in str(n):
+    if i.isdigit():
+        sum+=int(i)
+print(int(sum))
 
 
