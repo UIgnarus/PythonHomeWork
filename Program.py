8d9e0b9df76b6fec842414425fa6c82ddd5b028d
Ignarus <kitoroli98@gmail.com>
2022-11-09 22:42:51 +0300

    save

--- Program.py
+++ Program.py
@@ -1,7 +1,7 @@
 n = int(input())
-list = [round((1+1/i)**i, 2) for i in range(1, n + 1)]
+list = [(1+1/i)**i for i in range(1, n + 1)]
 sum = float(0)
 for i in range(len(list)):
     sum+= list[i]
-sum = round(float(sum), 2)
+sum = float(sum)
 print(sum)
