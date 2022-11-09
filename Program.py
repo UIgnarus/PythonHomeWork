97f57f6dac60d001d3c26849e9797693db55e250
Ignarus <kitoroli98@gmail.com>
2022-11-09 21:47:00 +0300

    save

--- Program.py
+++ Program.py
@@ -1,18 +1,7 @@
-"""
-Задайте список из n чисел последовательности
- $(1+\frac 1 n)^n$ и выведите на экран их сумму.
-
-Пример:
-- Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
-"""
-
-n = int(input("n = "))
-
-list = {i: round((1+1/i)**i, 2) for i in range(1, n+ 1)}
-
-print(list)
-
+n = int(input())
+list = [round((1+1/i)**i, 2) for i in range(1, n + 1)]
 sum = float(0)
-for i in range(1, len(list)+1):
+for i in range(len(list)):
     sum+= list[i]
-print(f"sum = {sum}")
+sum = round(float(sum), 2)
+print(sum)
