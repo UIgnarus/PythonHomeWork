cf92cea7370329c7fb31cb83011da2df7797cf23
Ignarus <kitoroli98@gmail.com>
2022-11-09 22:08:05 +0300

    save

--- Program.py
+++ Program.py
@@ -1,16 +1,8 @@
-"""
-Задайте список из N элементов,
- заполненных числами из промежутка [-N, N].
-  Найдите произведение элементов на указанных позициях. 
-Позиции хранятся в файле file.txt 
-в одной строке одно число.
-"""
-import random
-n = int(input("n = "))
 
-a = [random.randrange(-n,n) for i in range(n)]
-
-print(a)
-
-for i in range(n//2):
-    print(a[i]*a[(n-1)-i])
+n = int(input())
+position = [int(i) for i in input().split()]
+array = [int(i) for i in range(-n, n+1)]
+result = int(1)
+for i in range(len(position)):
+    result*= array[position[i]]
+print(int(result))
