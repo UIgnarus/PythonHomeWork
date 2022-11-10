7458b5b866041faa8b55e4a119ffb0e772a525f3
Ignarus <kitoroli98@gmail.com>
2022-11-10 23:32:39 +0300

    save

--- Program.py
+++ Program.py
@@ -16,5 +16,30 @@ def to_binary(x):
         x//=2
     result = ""
     for i in range(len(list)):
-        result+=str(list(-i-1))
-    return result
+        result+=str(list[-i-1])
+    return int(result)
+
+def functional_testing():
+    input_1 = 45
+    result_1 = 101101
+    if to_binary(input_1) == result_1:
+        print("Test#1 - OK")
+    else:
+        print("Test#1 - Fail")
+    input_2 = 3
+    result_2 = 11
+    if to_binary(input_2) == result_2:
+        print("Test#2 - OK")
+    else:
+        print("Test#2 - Fail")
+    input_3 = 2
+    result_3 = 10
+    if to_binary(input_3) == result_3:
+        print("Test#3 - OK")
+    else:
+        print("Test#3 - Fail")
+
+functional_testing()
+
+n = int(input())
+print(to_binary(n))
