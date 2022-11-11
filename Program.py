6c5dffd1a2745ef7f58d9a0f64b5abc649e09bb4
Ignarus <kitoroli98@gmail.com>
2022-11-11 23:26:30 +0300

    save

--- Program.py
+++ Program.py
@@ -9,12 +9,34 @@
 
 from math import pi
 
-
 def rounding_pi(d):
-    rounding = len(str(d)) - (len(str(int(d)))+1)
-    return round(pi, rounding)
+    """
+    10^{-1} ≤ d ≤10^{-10}
+    """
+    rounding = len(str(d)) - 2
+    print(d)
+    print(len(str(d)))
+    print(rounding)
+    result = int(pi * 10**rounding)/10**rounding
+    return result
 
+def functional_testing():
+    input_1 = 0.001
+    result_1 = 3.141
+    if rounding_pi(input_1) == result_1:
+        print("Test#1 - OK")
+    else:
+        print("Test#1 - Fail")
+    input_2 = 0.00001
+    result_2 = 3.14159
+    if rounding_pi(input_2) == result_2:
+        print("Test#2 - OK")
+    else:
+        print("Test#2 - Fail")
 
+functional_testing()
 
+# d = float(input())
+# print(rounding_pi(d))
 
         
